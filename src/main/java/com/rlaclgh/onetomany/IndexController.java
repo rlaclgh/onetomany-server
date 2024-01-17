fa83f6891fcda28bbc9c3f2a2071487c533358f6
rlaclgh <rlaclgh11@gmail.com>
2024-01-17 20:08:01 +0900

    first commit

--- src/main/java/com/rlaclgh/onetomany/IndexController.java
+++ src/main/java/com/rlaclgh/onetomany/IndexController.java
@@ -1,17 +1,10 @@
 package com.rlaclgh.onetomany;
 
 
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
-
-  @GetMapping("")
-  public ResponseEntity<String> test() {
-    return ResponseEntity.ok("hello");
-  }
 
 }
