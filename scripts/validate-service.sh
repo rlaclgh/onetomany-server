1f0bf4c8dd2c45cb9b737145134b9a6f873b1959
rlaclgh <rlaclgh11@gmail.com>
2024-01-23 19:01:17 +0900

    Edit build-and-upload.yml

--- scripts/validate-service.sh
+++ scripts/validate-service.sh
@@ -2,7 +2,7 @@
 
 while [ true ]
 do
-    if [ "$(curl -s http://localhost:8080)" = 'hello' ]
+    if [ "$(curl -s http://localhost:8080)" = 'hello1' ]
     then
         exit 0
     else
--- src/main/java/com/rlaclgh/onetomany/IndexController.java
+++ src/main/java/com/rlaclgh/onetomany/IndexController.java
@@ -10,6 +10,6 @@ public class IndexController {
 
   @GetMapping("")
   public ResponseEntity<String> test() {
-    return ResponseEntity.ok("hello");
+    return ResponseEntity.ok("hello1");
   }
 }
