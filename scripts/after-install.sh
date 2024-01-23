73ad87c436a1250c517e570ae6f59f839917c8ff
rlaclgh <rlaclgh11@gmail.com>
2024-01-23 17:10:43 +0900

    Edit after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,4 +1,5 @@
 pkill -f "java -jar /home/ubuntu/deploy/build/libs/onetomany-0.0.1-SNAPSHOT.jar"
 
+#pgrep -f "java -jar /home/ubuntu/deploy/build/libs/onetomany-0.0.1-SNAPSHOT.jar"
 
 nohup java -jar /home/ubuntu/deploy/build/libs/onetomany-0.0.1-SNAPSHOT.jar > nohup.log 2>&1 &
