a0ae3868edb5ad6b083aa6e7c90f33db27b03da6
rlaclgh <rlaclgh11@gmail.com>
2024-01-23 17:24:39 +0900

    Edit after-install.sh

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -1,5 +1,7 @@
 pkill -f "java -jar /home/ubuntu/deploy/build/libs/onetomany-0.0.1-SNAPSHOT.jar"
 
+
+sleep 5
 #pgrep -f "java -jar /home/ubuntu/deploy/build/libs/onetomany-0.0.1-SNAPSHOT.jar"
 
 nohup java -jar /home/ubuntu/deploy/build/libs/onetomany-0.0.1-SNAPSHOT.jar > nohup.log 2>&1 &
--- src/main/java/com/rlaclgh/onetomany/IndexController.java
+++ src/main/java/com/rlaclgh/onetomany/IndexController.java
@@ -10,6 +10,6 @@ public class IndexController {
 
   @GetMapping("")
   public ResponseEntity<String> test() {
-    return ResponseEntity.ok("hello5");
+    return ResponseEntity.ok("hello6");
   }
 }
